adc6703149a51a3dc02a12cc82e4af9ef7ab5f7c
Kunal Khullar <kunalkhullar5012@gmail.com>
2021-03-20 21:15:20 +0530

    cardflip added

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -3,6 +3,12 @@ import Row from 'react-bootstrap/Row'
 import Button from 'react-bootstrap/Button'
 import './Card.css'
 const Card = (props) => {
+    const flipCard = ()=>{
+        var card = document.querySelector('#item1');
+        card.classList.toggle('is-flipped');
+    }
+    
+
     console.log(props.data)
     return <>
         <div className="row allcards">
@@ -11,7 +17,12 @@ const Card = (props) => {
 
                     <div className="cardContainer">
                         <div className="itemcards front">
-                            <div className="itemImage"></div>
+                            <div className="scene">
+                            <div className="card" id='item1' onClick={flipCard}>
+                            <div className="itemImage card__face card__face--front"></div>
+                            <div className="back card__face card__face--back"></div>
+                            </div>
+                            </div>
                             <div className="details">
                                 <h2 className="itemName">{ele.itemName}</h2>
                                 <Row className="r1">
@@ -31,13 +42,13 @@ const Card = (props) => {
                                     <h2 className="c4">{ele.dimension}</h2>
                                 </Row>
                                 <Row className="r1">
-                                    <h2 className="c3">Rs.3000</h2>
+                                    <h2 className="c3 i1">Rs.3000</h2>
                                     <Button className="mybtn btn-success">Purchase</Button>
                                 </Row>
                             </div>
 
                         </div>
-                        <div className="back"></div>
+                        
                     </div>
 
                 )
